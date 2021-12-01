2ecc4092e2c9264f350e9d6824229c530634267b
glitch-opt <80940081+glitch-opt@users.noreply.github.com>
2021-11-30 23:07:29 -0700

    Started ml model api

--- runModel.py
+++ runModel.py
@@ -1,15 +1,23 @@
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pickle
 
 app = FastAPI()
 
-@app.on_event("startup")
-def load_model():
-    global model
-    model = pickle.load(open("model", "rb"))
+# @app.on_event("startup")
+# def load_model():
+#     global model
+#     model = pickle.load(open("model", "rb"))
 
-@app.get('/')
-def index():
-    return {'message': 'This is the homepage of the API '}
-server.run_app(app=app)
+# @app.get('/')
+# def index():
+#     return {'message': 'This is the homepage of the API '}
+# server.run_app(app=app)
+class Person(BaseModel):
+    
+
+@app.post('/predict')
+async def assign_group(person: Person):
+    data = person.dict()
+    model = model = pickle.load(open('ml_model.pkl', 'rb'))
